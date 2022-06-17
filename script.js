a23e80c5fc2efc6d1175b53a144b9ca6fef5a04f
belleomoraes <iolliveira.moraes@gmail.com>
2022-06-17 18:47:24 -0300

    com gifs corretos

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
+//definir numero de cartas
 let numCartas;
 while (numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0) {
   numCartas = Number(prompt("Com quantas cartas você quer jogar?"));
 }
 
-let jogos = `<div class="jogo">
-<div class="cards" onclick="virar(this)">
-<div class="front"><img src="imagens/front.png"/></div>
-</div>`;
-
-let contador = 0;
-
 let gifs = [
-`imagens/bobrossparrot.gif`,
-`imagens/explodyparrot.gif`,
-`imagens/fiestaparrot.gif`,
-`imagens/metalparrot.gif`,
-`imagens/revertitparrot.gif`,
-`imagens/unicornparrot.gif`,
-`imagens/tripletsparrot.gif`
+  `imagens/bobrossparrot.gif`,
+  `imagens/explodyparrot.gif`,
+  `imagens/fiestaparrot.gif`,
+  `imagens/metalparrot.gif`,
+  `imagens/revertitparrot.gif`,
+  `imagens/unicornparrot.gif`,
+  `imagens/tripletsparrot.gif`,
 ];
 
-const distribuicao = document.querySelector(".distribuicao");
-while (contador < numCartas) {
-    distribuicao.innerHTML += jogos;
-  contador++;
+//criando um baralho aleatório
+let jogos = [];
+let i = 0;
+function criarBaralhoAleatorio() {
+  for (let i = 0; jogos.length <= numCartas/2; i++) {
+    jogos.push(`<div class="cards" onclick="virar(this)">
+    <div class="back escondido"><img src="${gifs[i]}"/></div>
+    <div class="front"><img src="imagens/front.png"/></div>
+    </div>`);
+  const distribuicao = document.querySelector(".distribuicao");
+  distribuicao.innerHTML += jogos[i].concat(jogos[i]);
+  }
+  jogos.sort(comparador);
 }
 
+criarBaralhoAleatorio();
+
+function comparador() {
+  return Math.random() - 0.5;
+}
+//clicar na carta
+
+let cardsClicados = [];
 function virar(elemento) {
-    let imagemback = `
-    <div><img src="${gifs[Math.ceil(Math.random()*6)]}"/></div>
-    `
-    document.querySelector(".front").classList.add("escondido");
-    document.querySelector(".cards").innerHTML = imagemback;
+  let cardVirado = document.querySelector(".front .escondido");
+  if (cardVirado !== null) {
+    let cardVirado2 = cardVirado.querySelector(".back");
+    cardVirado.classList.remove("escondido");
+    cardVirado2.classList.add("escondido");
+  }
+  elemento.querySelector(".front").classList.add("escondido");
+  elemento.querySelector(".back").classList.remove("escondido");
+  cardsClicados.push(elemento);
+  //isso tem que acontecer ate cardsClicados.length === 2
 }
-
-
-
-
